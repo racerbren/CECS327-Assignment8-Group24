d7dbbbf7164d0afd9bc4714066b3760119445d5a
Brenden Bilson <brendenbilson5@gmail.com>
2024-12-05 22:20:36 -0800

    Update client side queries

--- client.py
+++ client.py
@@ -11,17 +11,23 @@ def main():
     socket.connect((serverIP, int(serverPort)))
     while True:
         # Send message to server
-        message = input('Enter message to send to server: ')
-        print(f"Sending: {message}")
-        socket.sendall(message.encode('utf-8'))
+        message = input("1. What is the average moisture inside my kitchen fridge in the past three hours?\n"
+                        "2. What is the average water consumption per cycle in my smart dishwasher?\n"
+                        "3. Which device consumed more electricity among my three IoT devices (two refrigerators and a dishwasher)?\n")
+        if int(message) not in range(1, 4):
+            print("Sorry, this query cannot be processed. Please try one of the following: \n")
+        
+        else:
+            print(f"Sending: {message}")
+            socket.sendall(message.encode('utf-8'))
 
-        if message.upper() == "STOP":
-            print(f"Server stopped.")
-            break
+            if message.upper() == "STOP":
+                print(f"Server stopped.")
+                break
 
-        # Receive message from server
-        data = socket.recv(1024)
-        print(f"Received: {data.decode('utf-8')}")
+            # Receive message from server
+            data = socket.recv(1024)
+            print(f"Received: {data.decode('utf-8')}")
 
 if __name__ == '__main__':
     main()  
--- server.py
+++ server.py
@@ -36,7 +36,7 @@ def main():
             print(f"Serving ending...")
             break
         
-        elif data == "TBD":
+        elif data == "1":
             total = 0
             i = 0
             for item in col.find({'time': {"$gt": datetime.now(timezone.utc) - timedelta(hours=3)},
@@ -45,6 +45,9 @@ def main():
                 i += 1
             average = round((Decimal(total / i) / Decimal(40.0)) * 100, 2)
             data = f"Average moisture of kitchen fridge over past 3 hours: {average}%\n"
+
+        elif data == "2":
+            print()
 
         #Send and encode message to client
         print(f"Server sending: {data}")
