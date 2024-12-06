39b0889251e4fd8e39a60c8718332edb2a0138d3
Brenden Bilson <brendenbilson5@gmail.com>
2024-12-05 22:43:10 -0800

    finished query 2

--- server.py
+++ server.py
@@ -36,6 +36,7 @@ def main():
             print(f"Serving ending...")
             break
         
+        #Average humidity of fridge over last 3 hours
         elif data == "1":
             total = 0
             i = 0
@@ -46,8 +47,35 @@ def main():
             average = round((Decimal(total / i) / Decimal(40.0)) * 100, 2)
             data = f"Average moisture of kitchen fridge over past 3 hours: {average}%\n"
 
+        #Average gallons per dishwasher cycle  (2 hours long cycle)
         elif data == "2":
-            print()
+            total = 0
+            i = 0
+            for item in col.find({'time': {"$gt": datetime.now(timezone.utc) - timedelta(hours=2)},
+                                  'payload.asset_uid': "nw4-asg-55o-ug3"}):
+                total += Decimal(item["payload"]["Water Consumption Sensor"])
+                i += 1
+            total = round(Decimal(total / Decimal(36.0)), 2)
+            data = f"Average water consumption: {total} gallons per cycle\n"
+
+        #Total energy consumption across 3 devices
+        elif data == "3":
+            fridge1 = 0
+            dishwasher = 0
+            fridge2 = 0
+
+            #Fridge 1
+            for item in col.find({'time': {"$gt": datetime.now(timezone.utc) - timedelta(hours=3)},
+                                  'payload.asset_uid': "n43-l4s-165-p2l"}):
+                fridge1 += Decimal(item["payload"]["Ammeter"])
+            #Dishwasher
+            for item in col.find({'time': {"$gt": datetime.now(timezone.utc) - timedelta(hours=1)},
+                                  'payload.asset_uid': "nw4-asg-55o-ug3"}):
+                dishwasher += Decimal(item["payload"]["Ammeter"])
+            #Fridge 2
+            for item in col.find({'time': {"$gt": datetime.now(timezone.utc) - timedelta(hours=1)},
+                                  'payload.asset_uid': "4a9-8so-qj3-7a1"}):
+                fridge2 += Decimal(item["payload"]["Ammeter"])
 
         #Send and encode message to client
         print(f"Server sending: {data}")
